4c7178b2fc1546a615d74232178d6597c8f7fd67
Ayoub Anjaimi <aanjaimi@e3r8p15.1337.ma>
2023-08-01 13:19:08 +0100

    branch created and sitting the front authentication page

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,7 +1,10 @@
+import Background from '@/components/Background'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <Background />
+    </>
   )
 }
