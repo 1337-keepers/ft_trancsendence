8269eab78ad0213593ade00f99bec29d760551cc
Aanjaimi <ayoub.anjaimi99@gmail.com>
2023-09-04 13:50:22 +0100

    fix: changing the landingpage from grid-rows to grid-cols

--- Frontend/src/components/Background.tsx
+++ Frontend/src/components/Background.tsx
@@ -3,9 +3,9 @@ import Card from './Card';
 
 const Background = () => {
   return (
-    <div className="w-screen h-screen grid grid-rows-3">
+    <div className="w-screen h-screen grid grid-cols-3">
         <div className="border rounded-br-full w-[250px] h-[250px] bg-[#8D8DDA] blur-[150px]"></div>
-        <div className="flex justify-center"><Card /></div>
+        <div className="flex justify-center mt-[20vh]"><Card /></div>
         <div className="flex justify-end items-end"><div className="border rounded-tl-full w-[250px] h-[250px] bg-[#ABD9D9] blur-[150px]"></div></div>
     </div>
   )
--- Frontend/src/components/Card.tsx
+++ Frontend/src/components/Card.tsx
@@ -8,18 +8,14 @@ import { useRouter } from 'next/router';
 const Card = () => {
   const router =  useRouter();
   return (
-    <div className="border border-black	rounded-[63px] bg-[#d9d9d933] w-[350px] h-[420px] sm:w-[541px] sm:h-[385px] flex flex-col items-center justify-center font-['outfit'] text-white">
+    <div className="border border-black rounded-[63px] bg-[#d9d9d933] w-[350px] h-[420px] sm:w-[541px] sm:h-[385px] flex flex-col items-center justify-center font-['outfit'] text-white">
       <div className="w-[65px] h-[84px] flex justify-center mb-[40px]">
         <Image src={"/logo.png"} alt="image" width={500} height={500}/>
       </div>
       <div className="text-[30px] mb-[20px] font-bold">Welcome to PongYo</div>
       <div className="text-[15px] ml-[50px] mr-[50px] mb-[20px]">Step into the world of competitive ping pong and experience the excitement of real-time multiplayer matches right from the comfort of your own device</div>
       <div className="rounded-full w-[125px] h-[45px] flex items-center justify-center bg-gradient-to-r from-[#8d8dda80] to-[#ABD9D980]">
-        {/* <button className="mr-[5px]"> */}
-          
-          {/* <a href="http://localhost:3000/oauth/callback">Sign in with</a> */}
-          <button onClick={() => router.push('http://localhost:3000/oauth/callback')}>Sign in with </button>
-        {/* </button> */}
+        <button onClick={() => router.push('http://localhost:3000/oauth/callback')}>Sign in with </button>
         <div className="mt-[3px] w-[21px] h-[21px] flex justify-center">
           <Image src={"/42.png"} alt="image" width={500} height={500}/>
         </div>
